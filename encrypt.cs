13f03e13bb92d12d38b2a479d8fbae08121b0080
stealthscripter <nicolasayele@gmail.com>
2024-01-07 12:18:41 +0300

    obj file

--- encrypt.cs
+++ encrypt.cs
@@ -1,3 +1,58 @@
 class encrypt{
     
+
+
+    private List<char> list;
+    private List<char> shuffleList;
+
+    private char character;
+    private string line;
+    private char[] letters;
+    private char[] secretLetters;
+
+    // public encrypt(){
+
+    // }
+
+    private void askQuestion(){
+        while(true){
+            Console.WriteLine("What do you want");
+            Console.WriteLine("(N)ewkey , (G)etKey , (E)ncrypt , (D)ecrypt , (Q)uite ");
+            char response =  char.Parse(Console.ReadLine());
+
+            switch(response){
+                case 'N':
+                    // Newkey();
+                    break;
+                case 'G':
+                    // GetKey();
+                    break;
+                case 'E':
+                    // Encrypt();
+                    break;
+                case 'D':
+                    // Decrypt();
+                    break;
+                case 'Q':
+                    // Quite();
+                    break;
+                default :
+                    Console.WriteLine("Invalid Answer :(");
+                    break;
+            }
+        }
+    }
+
+    private void NewKey(){
+        character = ' ';
+        list.Clear();
+        shuffleList.Clear();
+
+        for(int i = 32;i < 127;i++){
+            list.Add(character);
+            character++;
+        }
+        
+
+    }
 }
